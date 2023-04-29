f5086c10495984be4bca75b81d0a5bf87fa18a26
salom12 <keepgoing.ahmed@gmail.com>
2023-04-29 23:24:25 +0300

    implement db handler

---
+++ internal/database/db.go
@@ -0,0 +1,35 @@
+package database
+
+type row struct {
+	ID uint64
+}
+
+type DbHandler struct {
+}
+
+func (db DbHandler) GetByID(id uint64) (record *row) {
+	return nil
+}
+
+func (db DbHandler) Insert(updatedRow any) (id uint64) {
+	return 1
+}
+
+func (db DbHandler) Update(id uint64, updatedRow any) (success bool) {
+	return true
+}
+
+func (db DbHandler) Delete(id uint64) (success bool) {
+	return true
+}
+
+var db *DbHandler
+
+func InitDB() error {
+	db = &DbHandler{}
+	return nil
+}
+
+func GetDB() *DbHandler {
+	return db
+}
