1e42cc11f3fb8a57abbafcbc2da7cf6ac2e9ab64
salom12 <keepgoing.ahmed@gmail.com>
2023-04-29 23:09:45 +0300

    update cache struct

--- pkg/memcache/memcache.go
+++ pkg/memcache/memcache.go
@@ -2,23 +2,39 @@ package memcache
 
 import (
 	"fmt"
+	"reflect"
 	"sync"
+	"time"
 )
 
 type Cache struct {
 	Store
-	data    map[string]any
+	data    map[string]CacheValue
 	maxSize int
 	mutex   sync.Mutex
 
 	evictionPolicy EvictionPolicy
 }
 
-func NewCache(maxSize int) *Cache {
+type CacheValue struct {
+	data        any
+	accessCount int
+	lastAccess  time.Time
+}
+
+func NewCacheSimple(maxSize int) *Cache {
 	return &Cache{
-		data:           make(map[string]any),
+		data:           make(map[string]CacheValue),
 		maxSize:        maxSize,
 		evictionPolicy: &SimpleEviction{},
+	}
+}
+
+func NewCache(maxSize int, policy EvictionPolicy) *Cache {
+	return &Cache{
+		data:           make(map[string]CacheValue),
+		maxSize:        maxSize,
+		evictionPolicy: policy,
 	}
 }
 
@@ -28,10 +44,35 @@ func (c *Cache) Get(key string) (any, error) {
 
 	value, exists := c.data[key]
 	if !exists {
-		return value, fmt.Errorf("key not found in cache: %v", key)
+		return value.data, fmt.Errorf("key not found in cache: %v", key)
 	}
 
-	return value, nil
+	// update access count
+	value.accessCount++
+
+	// upadte last access
+	value.lastAccess = time.Now()
+
+	// Update cache
+	c.data[key] = value
+
+	return value.data, nil
+}
+
+func (c *Cache) GetString(key string) (string, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	value, err := c.Get(key)
+	if err != nil {
+		return "", err
+	}
+
+	if reflect.TypeOf(value).Kind() != reflect.String {
+		return "", fmt.Errorf("value is not a string")
+	}
+
+	return value.(string), nil
 }
 
 func (c *Cache) Set(key string, value any) {
@@ -42,7 +83,12 @@ func (c *Cache) Set(key string, value any) {
 		c.Evict()
 	}
 
-	c.data[key] = value
+	now := time.Now()
+	c.data[key] = CacheValue{
+		data:        value,
+		accessCount: 1,
+		lastAccess:  now,
+	}
 }
 
 func (c *Cache) Delete(key string) {
